335d546a6d578edd4e7d8b5ffc9ce239cdc07a0d
Sangeen-USTB <sangeenkhan2662@gmail.com>
2025-04-22 14:50:40 +0800

    Update __init__.py

--- src/biometric/__init__.py
+++ src/biometric/__init__.py
@@ -1,1 +1,16 @@
+"""
+Biometric sub‐package:
+ - preprocess_fingerprint
+ - extract_minutiae
+ - fuzzy_extractor
+"""
+from .preprocess       import preprocess_fingerprint
+from .minutiae         import extract_minutiae
+from .fuzzy_extractor  import fuzzy_extractor
+
+__all__ = [
+    "preprocess_fingerprint",
+    "extract_minutiae",
+    "fuzzy_extractor",
+]
 
