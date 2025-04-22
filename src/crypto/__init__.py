263cd66de483ee135dcf3ca3cbae7473ba50ef6b
Sangeen-USTB <sangeenkhan2662@gmail.com>
2025-04-22 14:51:08 +0800

    Update __init__.py

--- src/crypto/__init__.py
+++ src/crypto/__init__.py
@@ -1,1 +1,17 @@
+"""
+Crypto sub‐package:
+ - CA operations (generate_ca_keys, ca_sign_data, generate_did)
+ - User signature (generate_user_keys, user_sign_message, verify_user_signature)
+"""
+from .ca              import generate_ca_keys, ca_sign_data, generate_did
+from .user_signature  import generate_user_keys, user_sign_message, verify_user_signature
+
+__all__ = [
+    "generate_ca_keys",
+    "ca_sign_data",
+    "generate_did",
+    "generate_user_keys",
+    "user_sign_message",
+    "verify_user_signature",
+]
 
